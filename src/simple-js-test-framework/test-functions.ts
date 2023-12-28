b88761bdeaace46c1486de3a87ff44603f9dbddf
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-28 07:55:10 +0100

    test functions

--- src/simple-js-test-framework/test-functions.ts
+++ src/simple-js-test-framework/test-functions.ts
@@ -9,15 +9,48 @@ type TypeOf =
   | 'undefined'
   | 'symbol'
 
+// should have test case name and error message
+class CustomDescribeError extends Error {
+  testCaseName: string
+
+  constructor(message: string, testCaseName: string = '') {
+    super(message)
+    this.name = 'CustomDescribeError'
+    this.testCaseName = testCaseName
+  }
+}
+
 export function describe(testSuiteName: string, func: () => void) {
   console.log(`beginning test suite ${testSuiteName}`)
-  func()
+  try {
+    func()
+  } catch (error) {
+    if (error instanceof CustomDescribeError) {
+      console.error(
+        `failed running test suite ${testSuiteName} on test case ${error.testCaseName} with error message ${error.message}`
+      )
+      return
+    }
+
+    throw error
+  }
   console.log(`successfully completed test suite ${testSuiteName}`)
 }
 
 export function it(testCaseName: string, func: () => void) {
   console.log(`beginning test case ${testCaseName}`)
-  func()
+  try {
+    func()
+  } catch (error) {
+    if (error instanceof CustomDescribeError) {
+      throw new CustomDescribeError(
+        `${testCaseName} with error message ${error.message}`,
+        testCaseName
+      )
+    }
+
+    throw error
+  }
   console.log(`successfully completed test case ${testCaseName}`)
 }
 
@@ -26,14 +59,22 @@ export function expect(actual: unknown) {
     toExist() {
       const isValueExist = actual !== null && actual !== undefined
       if (!isValueExist) {
-        throw new Error(`Expected value to exist but got ${actual}`)
+        console.log(`expected value to exist but got ${JSON.stringify(actual)}`)
+        throw new CustomDescribeError(
+          `expected value to exist but got ${JSON.stringify(actual)}`
+        )
       }
       return true
     },
     toBe(expected: unknown) {
       const isValueEqual = actual === expected
       if (!isValueEqual) {
-        throw new Error(`Expected ${actual} to be ${expected}`)
+        console.log(
+          `expected ${JSON.stringify(actual)} to be ${JSON.stringify(expected)}`
+        )
+        throw new CustomDescribeError(
+          `expected ${JSON.stringify(actual)} to be ${expected}`
+        )
       }
 
       return true
@@ -41,6 +82,20 @@ export function expect(actual: unknown) {
     // one of the typeofs
     toBeType(expected: TypeOf) {
       const isTypeSame = typeof actual === expected
+      const typeOfActual = typeof actual
+      if (!isTypeSame) {
+        console.log(
+          `expected ${JSON.stringify(
+            actual
+          )} to be type ${expected} but got ${typeOfActual}`
+        )
+        throw new CustomDescribeError(
+          `expected ${JSON.stringify(
+            actual
+          )} to be type ${expected} but got ${typeOfActual}`
+        )
+      }
+      return true
     },
   }
 }
