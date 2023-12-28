c4918b756f45e63f7f2bb8b9d78b01493dac4402
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-28 08:45:48 +0100

    filter function

--- src/array-methods/index.ts
+++ src/array-methods/index.ts
@@ -22,17 +22,29 @@ interface Array<T> {
   ): U
 }
 
-Array.prototype.myMap = function <T>(
-  callback: (value: T, index: number, array: Array<T>) => U
+Array.prototype.myMap = function <U>(
+  callback: (value: unknown, index: number, array: Array<unknown>) => U
 ): Array<U> {
   const result = []
   for (let i = 0; i < this.length; i++) {
     result.push(callback(this[i], i, this)) // `this` is the array, how does it work? `this` works by referring to the array that is calling the function, since `myMap` is a method of the Array prototype, `this` refers to the array that is calling the method
   }
+
+  return result
 }
 
-Array.prototype.myFilter = function (callback) {
-  // Write your code here.
+Array.prototype.myFilter = function <T>(
+  callback: (value: T, index: number, array: Array<T>) => boolean
+): Array<T> {
+  const result = []
+
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      result.push(this[i])
+    }
+  }
+
+  return result
 }
 
 // Array.prototype.myReduce = function (callback, initialValue) {
