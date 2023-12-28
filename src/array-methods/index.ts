c75304c9cf235862d03794f18c235632a44123de
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-28 08:41:38 +0100

    my map

--- src/array-methods/index.ts
+++ src/array-methods/index.ts
@@ -31,9 +31,9 @@ Array.prototype.myMap = function <T>(
   }
 }
 
-// Array.prototype.myFilter = function (callback) {
-//   // Write your code here.
-// }
+Array.prototype.myFilter = function (callback) {
+  // Write your code here.
+}
 
 // Array.prototype.myReduce = function (callback, initialValue) {
 //   // Write your code here.
